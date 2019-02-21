67c2f7f7f5088d054088cc8e319ddfb417e0f17c
Samson Maconi <git@samsonmaconi.com>
2019-02-21 19:34:39 -0400

    Added a data service -Data generated from mockaroo based on provided schema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  dataAPIURL = 'https://my.api.mockaroo.com/epharma.json?key=3a972810?count=100'; // sample products dataset
+  productsData;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.productsData = this.loadProductsData();
+
+    // Logs sample dataset to console when it's loaded for debugging
+    this.getProductsData().subscribe(
+      data => {
+        console.log('"The data:"');
+        console.log(data);
+      },
+      error => console.error('There was an error retrieving the data', error),
+      () => console.log('Product Data retrieved!')
+    );
+  }
+
+  loadProductsData() {
+    return this.http.get(this.dataAPIURL);
+  }
+
+  getProductsData() {
+    return this.productsData;
+  }
+
 }
