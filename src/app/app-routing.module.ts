22d5cf74bca4031b5d690d98f909c6e91e13d2cd
Samson Maconi <git@samsonmaconi.com>
2019-03-09 13:09:03 -0400

    Merge conflicts resolved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
 import { DemoCheckComponent } from './demo-check/demo-check.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
-
-const routes: Routes = [
-
-  { 
-    path: '', component: HomeComponent
-   },
-   {
-     path : 'login', component:LoginComponent
-   },
-   {
-     path :'register',component:RegisterComponent
-   },
-  
-   {
-      path : 'demo-check', component : DemoCheckComponent
-    },     
-    {
-      path : 'signup-page', component : SignupPageComponent 
-    }
-
-];
-=======
 import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'product/:productId', component: ProductComponent }];
->>>>>>> home
+  { path: 'product/:productId', component: ProductComponent },
+  { path : 'login', component: LoginComponent },
+  { path : 'register', component: RegisterComponent },
+  { path : 'demo-check', component : DemoCheckComponent },
+  { path : 'signup-page', component : SignupPageComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-<<<<<<< HEAD
-import { ReactiveFormsModule} from '@angular/forms';
-=======
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { FlickityModule } from 'ngx-flickity';
->>>>>>> home
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,17 +12,13 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavCategoryComponent } from './nav-category/nav-category.component';
 import { NavComponent } from './nav/nav.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
 import { DemoCheckComponent } from './demo-check/demo-check.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
-=======
 import { SliderSingleComponent } from './slider-single/slider-single.component';
 import { SliderMultiComponent } from './slider-multi/slider-multi.component';
 import { ProductComponent } from './product/product.component';
->>>>>>> home
 
 @NgModule({
   declarations: [
@@ -36,33 +28,21 @@ import { ProductComponent } from './product/product.component';
     FooterComponent,
     NavCategoryComponent,
     NavComponent,
-<<<<<<< HEAD
-
     LoginComponent,
     RegisterComponent,
-
-    
     DemoCheckComponent,
-    
-    SignupPageComponent
-
-=======
+    SignupPageComponent,
     SliderSingleComponent,
     SliderMultiComponent,
     ProductComponent
->>>>>>> home
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     ReactiveFormsModule,
-<<<<<<< HEAD
-    
-=======
     HttpClientModule,
     FlickityModule
->>>>>>> home
   ],
   providers: [],
   bootstrap: [AppComponent]
