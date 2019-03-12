e78f77af39c54b46e9cb39d979d3f0533629cb51
Samson Maconi <git@samsonmaconi.com>
2019-03-12 01:15:36 -0300

    Created the Catalog View -Added the component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UploadPrescriptionComponent} from './upload-prescription/upload-prescription.component';
 import { ProductComponent } from './product/product.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'product/:productId', component: ProductComponent },
+  { path: 'product/', component: CatalogComponent },
+  { path: 'catalog/:category', component: CatalogComponent },
+  { path: 'catalog/', component: CatalogComponent },
   { path : 'login', component: LoginComponent },
   { path : 'register', component: RegisterComponent },
   { path : 'upload-prescription', component : UploadPrescriptionComponent }
