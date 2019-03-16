7d7570e4a3fe7800eb6a3ab1cd687ae18181d877
Samson Maconi <git@samsonmaconi.com>
2019-03-16 14:53:54 -0300

    Catalog Body partly functional - Added additional route for search navigation - Catalog displays items paginated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,9 +10,10 @@ import { CatalogComponent } from './catalog/catalog.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'product/:productId', component: ProductComponent },
-  { path: 'product/', component: CatalogComponent },
+  { path: 'product', component: CatalogComponent },
   { path: 'catalog/:category', component: CatalogComponent },
-  { path: 'catalog/', component: CatalogComponent },
+  { path: 'catalog/:category/:querystring', component: CatalogComponent },
+  { path: 'catalog', component: CatalogComponent },
   { path : 'login', component: LoginComponent },
   { path : 'register', component: RegisterComponent },
   { path : 'upload-prescription', component : UploadPrescriptionComponent }
