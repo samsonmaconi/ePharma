b8c3e2e00cee670cf8a396edc102b8efc96ed338
Samarth Vyomeshchandra Raval <sm978741@dal.ca>
2019-03-22 01:32:01 -0300

    Changes prescription  added

--- server/index.js
+++ server/index.js
@@ -1,20 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-
-
-const loginRegisterRoute = require('./routes/login-register-routes');
-const productRoutes = require('./routes/products');
-const adminRoutes = require('./routes/admin');
-
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const loginRegisterRoute = require("./routes/login-register-routes");
+const productRoutes = require("./routes/products");
+const adminRoutes = require("./routes/admin");
 const mongoose = require("mongoose");
 const prescriptionRoute = require("./routes/prescription-routes");
-
-
 const PORT = 1234;
+
 const DB_URI =
-  'mongodb+srv://epharma_admin:epharma_admin@cluster0-me8ny.mongodb.net/epharma?retryWrites=true';
+  "mongodb+srv://epharma_admin:epharma_admin@cluster0-me8ny.mongodb.net/epharma?retryWrites=true";
 const MONGOOSE_OPTIONS = {
   useNewUrlParser: true,
   socketTimeoutMS: 0,
@@ -23,15 +19,15 @@ const MONGOOSE_OPTIONS = {
 
 mongoose
   .connect(DB_URI, MONGOOSE_OPTIONS)
-  .then(() => console.log('Database Connected'))
+  .then(() => console.log("Database Connected"))
   .catch(ERR => console.log(ERR));
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.use('/api/products', productRoutes);
-app.use('/api/admin', adminRoutes);
-app.use("/api/user",loginRegisterRoute);
+app.use("/api/products", productRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/user", loginRegisterRoute);
 
-app.use("/api",prescriptionRoute);
+app.use("/api", prescriptionRoute);
 app.listen(PORT, () => console.log("Server listening at port: " + PORT));
--- src/app/prescription.model.ts
+++ src/app/prescription.model.ts
@@ -1,4 +1,4 @@
-export interface IPrescription{
+export interface IPrescription {
   orderNumber: number;
   name: string;
   email: string;
