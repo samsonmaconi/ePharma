6de8b69ecb79dd1ff977b3f59d7160ac78cff4c9
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2019-03-23 16:59:57 -0300

    adding admin module files

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-=======
+
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
->>>>>>> d0b05626666364964889c0771898d49eea9ac9a4
+
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminMainComponent } from './admin-main/admin-main.component';
@@ -23,12 +23,12 @@ import { OrderitemsComponent } from './orderitems/orderitems.component';
   imports: [
     CommonModule,
     AdminRoutingModule,
-<<<<<<< HEAD
-    NgbModule
-=======
+
+    NgbModule,
+
     FormsModule,
     ReactiveFormsModule
->>>>>>> d0b05626666364964889c0771898d49eea9ac9a4
+
   ],
   bootstrap: [AdminMainComponent],
 
